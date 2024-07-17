1046af4e2ca5b20f130420dd8ac6ca2dd09b6e92
Sharan S <147249297+Sharn03@users.noreply.github.com>
2024-07-17 18:41:33 +0530

    Update UserService.java

--- src/main/java/com/LoginPageTask/Service/UserService.java
+++ src/main/java/com/LoginPageTask/Service/UserService.java
@@ -1,5 +1,71 @@
 package com.LoginPageTask.Service;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.LoginPageTask.Entity.User;
+import com.LoginPageTask.Repository.UserRepo;
+
+@Service
 public class UserService {
 
+	@Autowired
+	private UserRepo userRepo;
+	
+	public List<User> getAllUser()
+	{
+		List<User> employeeList = userRepo.findAll();
+		return employeeList;
+	}
+	
+	public User getUserByEmailId(String mailId)
+	{
+		User user = userRepo.findByEmailId(mailId);
+		return user;
+	}
+	
+	
+	public User getUserById(int id)
+	{
+		//Declaring the employee obj to null 
+		User user = null;
+		
+		// checking if the id is null or not
+		if(Objects.nonNull(id))
+		{
+		 // here we are using Optional class to check where the object is present or not 
+		 Optional<User> optionalUser = userRepo.findById(id);
+		 
+		 if(optionalUser.isPresent())
+		 {
+			 user = optionalUser.get();
+		 }
+		
+		 else
+		 {
+			throw new RuntimeException("User id "+ id +" Not Found ");
+		 }
+		 
+		}
+		 
+		 return user;
+	}
+	
+	public void saveUser(User user)
+	{
+		//checking the arguments is null or not
+		if(Objects.nonNull(user))
+		{
+			
+		 userRepo.save(user);
+		
+		}
+	}
+	
+	
 }
